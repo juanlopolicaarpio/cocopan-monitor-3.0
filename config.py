3a1046d4d1ffb022d2e4bac9993b294c63618741
Juanlo Policarpio <juanloplicarpio@gmail.com>
2025-09-13 01:01:41 +0800

    made everything normal

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ class Config:
 
     # ---- Monitoring ----
     MONITOR_START_HOUR = int(os.getenv('MONITOR_START_HOUR', '0'))   # 6 
-    MONITOR_END_HOUR   = int(os.getenv('MONITOR_END_HOUR', '24'))    # 20 
+    MONITOR_END_HOUR   = int(os.getenv('MONITOR_END_HOUR', '20'))    # 20 
     CHECK_INTERVAL_MINUTES = int(os.getenv('CHECK_INTERVAL_MINUTES', '60'))
     TIMEZONE = os.getenv('TIMEZONE', 'Asia/Manila')
 
